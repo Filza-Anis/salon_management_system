54efaad51f8c50f8bdbf958e0cbf6902ed2d0071
Filza-Anis <96613728+Filza-Anis@users.noreply.github.com>
2022-02-06 20:24:03 +0500

    first commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => InitState();
+}
+
+class InitState extends State<SplashScreen>{
+@override
+  Widget build(BuildContext context){
+    return initWidget();
+  }
+
+  Widget initWidget(){
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: new Color(0xffF5591F),
+              gradient: LinearGradient(
+                colors: [(new Color(0xffF5591F)),(new Color(0xffF2861E))],
+                begin:Alignment.topCenter, 
+                 end:Alignment.bottomCenter
+                  )
+            ),
+          )
+        
+        
+        Center(
+          child: Container(
+            child:Image.asset("images/background.jpg"),
+          ),
+
+          )
+        ],
+      ),
+    );
+  }
+}
